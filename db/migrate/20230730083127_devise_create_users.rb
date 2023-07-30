422992de050b5300580978a434344c82d882e715
EC2 Default User <ec2-user@ip-172-31-33-245.ap-northeast-1.compute.internal>
2023-07-30 10:47:37 +0000

    [finish]userテーブル、devise:views、login,logout

--- db/migrate/20230730083127_devise_create_users.rb
+++ db/migrate/20230730083127_devise_create_users.rb
@@ -33,7 +33,8 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.1]
       # t.datetime :locked_at
 
       t.string :name, null: false
-      t.introduction
+      t.string :introduction
+
 
       t.timestamps null: false
     end
