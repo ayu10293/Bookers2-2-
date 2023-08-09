d48feb5396377bae227d9b43e6f0ad763f5c5de9
EC2 Default User <ec2-user@ip-172-31-33-245.ap-northeast-1.compute.internal>
2023-08-09 03:33:27 +0000

    [finish]バリデーション 一意性

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,12 @@
-class UserController < ApplicationController
+class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     @book = @user.book
   end
 
+  def index
+    @users = User.all
+  end
   def edit
     @user = User.find(params[:id])
   end
